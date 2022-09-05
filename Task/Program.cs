08e31c087fd7c38113094988e7c63794310d4844
George.S <piligrimius@mail.ru>
2022-09-05 15:08:43 +0300

    Добавлен метод сортировки

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
 // длина которых меньше либо равна 3 символа.
+
+string[] Array = {"hello", "2", "world", ":-)"}; 
+string[] sortArray = new string[Array.Length]; 
+
+void SortArray(string[] arr, string[] sortArr) 
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3)
+        {
+            sortArr[count] = arr[i];
+            count++;
+        }
+    }
+}
